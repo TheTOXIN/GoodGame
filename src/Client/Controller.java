c3b70e82a67a1b16882aa0c0c7f168f76f0ddcba
DarkOverlord2504 <kostia.vierieshchaghin@mail.ru>
2018-01-20 23:45:34 +0000

    Решён мердж конфликт

--- src/Client/Controller.java
+++ src/Client/Controller.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD:Client/Controller.java
-import javafx.event.EventHandler;
-import javafx.event.Event;
-import javafx.scene.input.KeyEvent;
-=======
 package Client;
 
 import java.awt.event.KeyListener;
@@ -10,14 +5,87 @@ import java.awt.event.KeyEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.Timer;
->>>>>>> ae256ee7de16ae6fe981ebcef40d278f78d88496:src/Client/Controller.java
 
-/*Хз как генериксы ваши работают в интерфейсах*/
-public class Controller implements EventHandler<KeyEvent>
+class Controller implements KeyListener, ActionListener
 {
-	@Override
-	public void handle(KeyEvent e)
+	public View v;
+
+	private Player player;
+	private Timer timer;
+	private boolean isUp, isLeft, isRight, isDown;
+	
+	public Controller(Player player, int delay)
 	{
-		System.out.println("Hello!");
+		this.player = player;
+		timer = new Timer(delay, this);
+	}
+	
+	public void start()
+	{
+		timer.start();
+	}
+	
+	public void stop()
+	{
+		timer.stop();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		if(isUp)
+			player.stepUp();
+		if(isLeft)
+			player.stepLeft();
+		if(isRight)
+			player.stepRight();
+		if(isDown)
+			player.stepDown();
+		v.repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{		
+		switch(e.getKeyCode())
+		{
+			case 87://w
+				isUp = true;
+				break;
+			case 65://a
+				isLeft = true;
+				break;
+			case 68://d
+				isRight = true;
+				break;
+			case 83://s
+				isDown = true;
+				break;
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		switch(e.getKeyCode())
+		{
+			case 87://w
+				isUp = false;
+				break;
+			case 65://a
+				isLeft = false;
+				break;
+			case 68://d
+				isRight = false;
+				break;
+			case 83://s
+				isDown = false;
+				break;
+		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
 	}
 }
--- src/Client/View.java
+++ src/Client/View.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD:Client/View.java
-import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.paint.Color;
-import javafx.scene.input.KeyCode;
-import javafx.animation.AnimationTimer;
-import javafx.scene.shape.Rectangle;
-=======
 package Client;
 
 import javax.swing.JComponent;
@@ -15,75 +7,48 @@ import java.awt.Point;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.Timer;
->>>>>>> ae256ee7de16ae6fe981ebcef40d278f78d88496:src/Client/View.java
 
-import java.util.Random;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class View extends Scene
+class View extends JComponent implements ActionListener
 {
-	//private ArrayList<Player> players;
-	private Rectangle mainPlayer;
-	private Group root;
-	//private HashMap<KeyCode, Boolean> keys;
+	private Timer timer;
+	private int repaintsPerSecond;
 
-	public View(Group root)
+	public View()
 	{
-		super(root);
-		mainPlayer = new Rectangle();
-		this.root.getChildren().add(mainPlayer);
-	
-		
+		repaintsPerSecond = 30;
+		timer = new Timer(1000 / repaintsPerSecond, this);
 	}
-
-	//@Override
-	/*public void start(Stage stage) throws Exception
+	
+	public void start()
 	{
-		int step = 2;
-		long delay = 1000 / 60;
-		Random r = new Random();
-		Color color = new Color(r.nextDouble(), r.nextDouble(), r.nextDouble(), 1);
-		
-		keys = new HashMap<>();
-		keys.put(KeyCode.UP, false);
-		keys.put(KeyCode.LEFT, false);
-		keys.put(KeyCode.RIGHT, false);
-		keys.put(KeyCode.DOWN, false);
-		
-		mainPlayer = new Rectangle(20, 20, color);
-		
-		root = new Group();
-		root.getChildren().add(mainPlayer);
-		
-		Scene scene = new Scene(root);
-		scene.setOnKeyPressed(event->
+		timer.start();
+	}
+	
+	public void stop()
+	{
+		timer.stop();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		repaint();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		g.setColor(Color.black);
+		Player p;
+		Point loc;
+		int s;
+		for(int i = 0; i < Main.players.size(); i++)
 		{
-			if(keys.get(event.getCode()) != null)
-				keys.put(event.getCode(), true);
-		});
-		scene.setOnKeyReleased(event->
-		{
-			if(keys.get(event.getCode()) != null)
-				keys.put(event.getCode(), false);
-		});
-		
-		AnimationTimer timer = new AnimationTimer() {
-            		@Override
-            		public void handle(long now)
-            		{
-				if(keys.getOrDefault(KeyCode.UP, false))
-					mainPlayer.setY(mainPlayer.getY() - step);
-				if(keys.getOrDefault(KeyCode.LEFT, false))
-					mainPlayer.setX(mainPlayer.getX() - step);
-				if(keys.getOrDefault(KeyCode.RIGHT, false))
-					mainPlayer.setX(mainPlayer.getX() + step);
-				if(keys.getOrDefault(KeyCode.DOWN, false))
-					mainPlayer.setY(mainPlayer.getY() + step);
-			}
-        	};
-        	timer.start();
-		
-		
-	}*/
+			p = Main.players.get(i);
+			loc = p.getLocation();
+			s = p.getSize();
+			g.setColor(p.getColor());
+			g.fillRect((int)loc.getX(), (int)loc.getY(), s, s);
+		}
+	}
 }
