a100d2939a05602b7cbeb931aae12955c1aa20a4
toxin <yuri.belousov.1998@gmail.com>
2018-08-28 17:04:06 +0300

    r refactor code style

--- src/com/sharaga/gg/utill/Settings.java
+++ src/com/sharaga/gg/utill/Settings.java
@@ -1,9 +1,9 @@
 package com.sharaga.gg.utill;
 
 public class Settings {
-    public final static int W = 20;
-    public final static int H = 20;
-    public final static int SIZE = 30;
-    public final static int PING = 30;
-    public final static int SPEED = 60;
+    public static final int W = 20;
+    public static final int H = 20;
+    public static final int SIZE = 30;
+    public static final int PING = 30;
+    public static final int SPEED = 60;
 }
